af3e96549a3714a98f27ec6bda68807ded5ba8ec
kalibsm <eleyilsidimohamed@gmail.com>
2026-04-18 05:44:31 +0000

    Add Render deployment configuration
    - requirements.txt with gunicorn and whitenoise
    - build.sh for collectstatic and migrate steps
    - settings.py: env-based SECRET_KEY/DEBUG/ALLOWED_HOSTS, whitenoise middleware and static storage

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -10,22 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7t7o1!0uw^_cv5@)u-wy1e$4uu4b2v1-cfkz9#ob8&y8cq%otu'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-7t7o1!0uw^_cv5@)u-wy1e$4uu4b2v1-cfkz9#ob8&y8cq%otu')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
 
 
 # Application definition
@@ -42,6 +40,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
